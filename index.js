3f05ba72c75b0bbe8103e94ee4cc08cccb67e97b
Kena Wakwoya <37274102+kena-wakwoya@users.noreply.github.com>
2021-08-13 17:14:06 +0300

    using middle ware in redux
    using middle ware in redux

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const redux = require("redux");
+const reduxLogger = require("redux-logger");
 
 const createStore = redux.createStore;
 const combineReducer = redux.combineReducers;
+const applyMiddleWare = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
+
 const BUY_CAKE = "BUY_CAKE";
 
 const BUY_ICECREAM = "BUY_ICECREAM";
@@ -55,11 +59,9 @@ const rootReducer = combineReducer({
   iceCream: iceCreamReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleWare(logger));
 console.log("initial state:", store.getState());
-const unsubscribe = store.subscribe(() =>
-  console.log("updated state: ", store.getState())
-);
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
